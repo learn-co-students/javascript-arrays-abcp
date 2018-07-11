3b3447f96a7889a2d35c5b129b346924a6427e97
Donna Cerrio <denique86@yahoo.com>
2018-07-11 01:07:11 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var addElementToBeginningOfArray = ["Boston", "New Mexico"]
+addElementToBeginningOfArray.unshift("Philadelphia")
+
+var destructivelyAddElementToBeginningOfArray
+var cities = ["New York", "SanFrancisco"];
+cities.unshift("Philadelphia")
